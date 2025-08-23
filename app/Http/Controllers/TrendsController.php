b842811e2b64fb343fa20009efd7c4fd821589e8
Gauro11 <milbertgaringa5@gmail.com>
2025-08-22 23:58:03 -0700

    qwe

--- app/Http/Controllers/TrendsController.php
+++ app/Http/Controllers/TrendsController.php
@@ -8,26 +8,41 @@ use App\Models\MorbidityMortalityManagement;
 
 class TrendsController extends Controller
 {
+   public function index()
+{
+   $morbidityCases = DB::table('morbidity_mortality_management')
+    ->whereRaw('LOWER(category) = ?', ['morbidity'])
+    ->distinct()
+    ->pluck('case_name')
+    ->toArray();
+
+$mortalityCases = DB::table('morbidity_mortality_management')
+    ->whereRaw('LOWER(category) = ?', ['mortality'])
+    ->distinct()
+    ->pluck('case_name')
+    ->toArray();
+
+}
+
+
+
    public function getTrendData(Request $request)
-    {
-        $category = $request->get('category'); // morbidity or mortality
-        $caseName = $request->get('case_name');
+{
+    $category = $request->get('category'); // morbidity or mortality
+    $caseName = $request->get('case_name');
 
-        $query = DB::table('morbidity_mortality_management')
-            ->selectRaw('DATE(date) as date, SUM(male_count + female_count) as total')
-            ->whereRaw('LOWER(category) = ?', [$category]);
+    $data = DB::table('morbidity_mortality_management')
+        ->selectRaw('`date` as date, SUM(male_count + female_count) as total')
+        ->where('category', $category)
+        ->where('case_name', $caseName)
+        ->groupBy('date')
+        ->orderBy('date')
+        ->get();
 
-        if ($caseName) {
-            $query->where('case_name', $caseName);
-        }
-
-        $data = $query->groupBy('date')->orderBy('date')->get();
-
-        return response()->json([
-            'success' => true,
-            'labels' => $data->pluck('date'),
-            'values' => $data->pluck('total'),
-        ]);
-    }
+    return response()->json([
+        'labels' => $data->pluck('date'),
+        'values' => $data->pluck('total'),
+    ]);
+}
 
 }
