5bb5bb0e7067363a8649c6aceb3e1440dc3d1e9a
Gauro11 <milbertgaringa5@gmail.com>
2025-08-27 21:30:48 -0700

    qwe

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'PreventBackHistory' => \App\Http\Middleware\PreventBackHistory::class, 
         
     ];
 }
