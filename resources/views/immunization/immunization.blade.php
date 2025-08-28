0bb0c7aeec6d80af7103d9956e55711394a29643
Gauro11 <milbertgaringa5@gmail.com>
2025-08-27 18:57:13 -0700

    qwe

--- resources/views/immunization/immunization.blade.php
+++ resources/views/immunization/immunization.blade.php
@@ -2332,6 +2332,44 @@
 
     @include('staff.js')
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm px-3">
+    <a class="sidebar-toggle js-sidebar-toggle me-3">
+        <i class="hamburger align-self-center"></i>
+    </a>
+
+    <div class="collapse navbar-collapse justify-content-end">
+        <ul class="navbar-nav align-items-center">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    @php
+                        $user = \Illuminate\Support\Facades\Auth::user();
+                    @endphp
+
+                    @if ($user)
+                        <span class="fw-semibold text-dark me-2">
+                            {{ $user->last_name }}, {{ $user->first_name }}
+                        </span>
+                        <img src="https://ui-avatars.com/api/?name={{ urlencode($user->first_name . ' ' . $user->last_name) }}&background=0D8ABC&color=fff" 
+                             class="rounded-circle" width="32" height="32" alt="User Avatar">
+                    @endif
+                </a>
+
+                <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="userDropdown">
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="dropdown-item text-danger">
+                                <i class="me-2" data-feather="log-out"></i>Log Out
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
 </body>
 
 </html>
