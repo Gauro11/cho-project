5e0fb108267f81e5ac46c30554d6bd1867837e5d
Gauro11 <milbertgaringa5@gmail.com>
2025-08-31 07:23:36 -0700

    rty

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -131,7 +131,7 @@
             <div class="block mb-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember Me') }}</span>
                 </label>
             </div>
 
