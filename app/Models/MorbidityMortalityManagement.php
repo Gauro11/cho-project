2922b8685dcd6b44cdd89d42e41ea48dd421e818
Gauro11 <milbertgaringa5@gmail.com>
2025-09-05 03:39:37 -0700

    qwe

--- app/Models/MorbidityMortalityManagement.php
+++ app/Models/MorbidityMortalityManagement.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class MorbidityMortalityManagement extends Model
 {
     use HasFactory;
+     protected $table = 'morbidity_mortality_management'; // add this if your table is singular
 
     protected $fillable = [
         'category',
