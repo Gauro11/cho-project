06f170449629049af9ed5a6faec73a806d15deb0
Gauro11 <milbertgaringa5@gmail.com>
2025-09-05 04:11:55 -0700

    qwe

--- app/Imports/MorbidityImport.php
+++ app/Imports/MorbidityImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\MorbidityMortalityManagement;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date as ExcelDate;
 
 class MorbidityImport implements ToModel, WithHeadingRow
 {
@@ -13,14 +14,19 @@ class MorbidityImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
+        $date = $row['date'];
+
+        // ✅ If it's a number, convert from Excel serial to Y-m-d
+        if (is_numeric($date)) {
+            $date = ExcelDate::excelToDateTimeObject($date)->format('Y-m-d');
+        }
+
         return new MorbidityMortalityManagement([
-         'category' => $row ['category'],
-         'case_name'=> $row ['case_name'],
-         'date'=> $row      ['date'],
-         'male_count'=> $row ['male_count'],
-         'female_count'=> $row ['female_count'],
+            'category'     => $row['category'],
+            'case_name'    => $row['case_name'],
+            'date'         => $date,
+            'male_count'   => $row['male_count'],
+            'female_count' => $row['female_count'],
         ]);
     }
 }
-
-
