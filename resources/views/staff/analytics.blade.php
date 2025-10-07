5942dfed3621e54b23ef7fba25fc7ea465e95d10
Gauro11 <milbertgaringa5@gmail.com>
2025-10-06 21:33:47 -0700

    a

--- resources/views/staff/analytics.blade.php
+++ resources/views/staff/analytics.blade.php
@@ -574,7 +574,7 @@
             createChart("mortalityCasesChart", getChronologicalTopCases(mortalityData));
         });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 
 </html>
